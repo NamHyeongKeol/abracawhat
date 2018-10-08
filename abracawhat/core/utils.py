f83c2bbbd634bc652545de5b9a7e47c7c0fc6f5c
NamHyeongKeol <grey_pastor@naver.com>
2018-10-09 00:19:12 +0900

    utils

---
+++ abracawhat/core/utils.py
@@ -0,0 +1,17 @@
+class StringUtil:
+    EMPTY = ''
+    SPACE = ' '
+    ZERO = '0'
+    ONE = '1'
+    TWO = '2'
+
+
+class NumUtil:
+    NEGATIVE_ONE = -1
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+
+
+class FieldUtil:
+    MAX_NUMBER_OF_USERS_IN_GAME = 5
